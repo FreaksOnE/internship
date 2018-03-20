dda9ef758c3826981497a900f14b4397f1016b28
FreaksOnE <samanasady@gmail.com>
2018-03-20 11:05:12 +0330

    update

---
+++ nativeApp/chatApp/src/auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+	clientId: "xLdhXhO_gp2bCFQ0B5cJTcqJqqpq_hBI",
+	domain: "chat-demo-app.eu.auth0.com",
+	callbackUrl: "/callback",
+	apiUrl: "http://5.160.218.90:3000/api",
+};
