b4e41d25c9d5c4a6c9932b6fe3e2729b18fe92b5
FreaksOnE <samanasady@gmail.com>
2018-03-20 11:52:39 +0330

    update

--- chatApp/src/auth/auth0-variables.js
+++ chatApp/src/auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
 	clientId: "xLdhXhO_gp2bCFQ0B5cJTcqJqqpq_hBI",
 	domain: "chat-demo-app.eu.auth0.com",
-	callbackUrl: "http://localhost:8080/callback",
+	callbackUrl: "http://5.160.218.90/callback",
 	apiUrl: "http://5.160.218.90:3000/api",
 };
--- nativeApp/chatApp/src/auth/auth0-variables.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_CONFIG = {
-	clientId: "xLdhXhO_gp2bCFQ0B5cJTcqJqqpq_hBI",
-	domain: "chat-demo-app.eu.auth0.com",
-	callbackUrl: "/callback",
-	apiUrl: "http://5.160.218.90:3000/api",
-};
