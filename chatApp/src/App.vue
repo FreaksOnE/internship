b64499b84fc5bc5e7c91d0c9aed6b9543d0527d0
FreaksOnE <samanasady@gmail.com>
2018-03-22 12:49:06 +0430

    update

--- chatApp/src/App.vue
+++ chatApp/src/App.vue
@@ -64,9 +64,9 @@ export default {
 <style>
 @font-face {
   font-family: "Gubblebum Blocky";
-  src: url("/static/GUBBLO___.eot?") format("eot"), url("/static/GUBBLO___.woff") format("woff"),
-    url("/static/GUBBLO___.ttf") format("truetype"),
-    url("/static/GUBBLO___.svg#Gubblebum-Blocky") format("svg");
+  src: url("./assets/fonts/GUBBLO___.eot?") format("eot"), url("./assets/fonts/GUBBLO___.woff") format("woff"),
+    url("./assets/fonts/GUBBLO___.ttf") format("truetype"),
+    url("./assets/fonts/GUBBLO___.svg#Gubblebum-Blocky") format("svg");
   font-weight: normal;
   font-style: normal;
 }
